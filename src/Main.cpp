e73266884a9111e298db157d342dc5e7e5eae913
Alexandre Fresneda Riveira <alexandre@objectdata.com.br>
2019-03-25 18:24:11 -0300

    config file

--- src/Main.cpp
+++ src/Main.cpp
@@ -7,58 +7,57 @@
 #include <charon/mvm>
 #include <charon/base>
 
+#include <QJsonObject>
+
 using mvm     = charon::mvm;
 using Log     = charon::Log;
 using service = charon::service;
 
-
-
 int main(int argc, char *argv[])
 {
     mvm::init(argc, argv);
-    bool service  = true;
-    int  threadNum = os::cores();
-    int  port = 2345;
 
-    // parse args
-    int opt;
-    const char *str = "t:l:p:";
-    while ((opt = getopt(argc, argv, str))!= -1)
-    {
-        switch (opt)
-        {
-            case 't':
-            {
-                threadNum = atoi(optarg);
-                break;
-            }
-            case 'p':
-            {
-                port = atoi(optarg);
-                break;
-            }
-            default: break;
-        }
+    bool service       = true;
+    int  threadNum     = 4;
+    int  port          = 2345;
+    charon::string pid = "titan.pid";
+
+    QFile config("config/titan.json");
+    if( config.exists() ) {
+      config.open(QIODevice::ReadOnly);
+      QJsonParseError * error = new QJsonParseError();
+      QJsonDocument json = QJsonDocument::fromJson(config.readAll(), error);
+      if( error->error != 0 ) {
+        qDebug() << error->errorString();
+        throw;
+      }
+      QJsonObject object = json.object();
+      port      = object.value("port").toInt();
+      threadNum = object.value("threads").toInt();
+      pid       = object.value("pid").toString().toLocal8Bit();
+      service   = object.value("service").toBool();
+    } else {
+      qDebug() << "config/titan.json file not exists";
     }
 
-      // SERVICES
-      if( service && os::exists("app/services")) {
-        charon::service::load("app/services");
-      }
+    // SERVICES
+    if( service && os::exists("app/services")) {
+      charon::service::load("app/services");
+    }
 
-      if( os::exists("logs") ) {
-        Log log = Log("logs/titan.log");
-        log.info("iniciando titan");
-        if( service ) {
-          if( os::exists("app/services") ) {
-            log.info("services started");
-          } else {
-            log.info("services dir not exists");
-          }
+    if( os::exists("logs") ) {
+      Log log = Log("logs/titan.log");
+      log.info("iniciando titan");
+      if( service ) {
+        if( os::exists("app/services") ) {
+          log.info("services started");
         } else {
-          log.info("services not enable");
+          log.info("services dir not exists");
         }
-        log.dump();
+      } else {
+        log.info("services not enable");
+      }
+      log.dump();
     }
 
     EventLoop mainLoop;
